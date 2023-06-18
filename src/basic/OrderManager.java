7b07467f34904aab39c301110ed4348c064abad1
Kitae's Github <aqua0405@gmail.com>
2023-06-19 01:22:23 +0900

    Update OrderManager.java

--- src/basic/OrderManager.java
+++ src/basic/OrderManager.java
@@ -2,13 +2,13 @@ package basic;
 
 public class OrderManager
 {
-	private HyundaiMaker maker;
-//	private KiaMaker maker;
+//	private HyundaiMaker maker;
+	private KiaMaker maker;
 
 	public OrderManager() {
 		super();
-		maker = new HyundaiMaker();	// 생성자를 통한 생성 (의존)
-//		maker = new KiaMaker();	// 생성자를 통한 생성 (강결합)
+//		maker = new HyundaiMaker();	// 생성자를 통한 생성 (의존)
+		maker = new KiaMaker();	// 생성자를 통한 생성 (강결합)
 	}
 	
 	public void order() {
